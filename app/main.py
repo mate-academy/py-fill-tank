b6bb88fdbbdbcdab6116b00bd2e57f4778b1d65b
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-02-02 14:02:23 +0200

    Fill tank

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
-# TODO: add initial code
-def hello_world():
-    return "Hello, world!"
+def fill_tank(customer: dict, fuel_price: float, amount: float = None):
+    can_buy = customer["money"] / fuel_price
+    free_space = customer["vehicle"]["max_tank_capacity"] - customer["vehicle"]["fuel_remains"]
+    if not amount:
+        amount = free_space
+    required_amount = round(min(can_buy, free_space, amount), 1)
+    if required_amount < 2:
+        return
+    customer["vehicle"]["fuel_remains"] += required_amount
+    customer["money"] -= round(required_amount * fuel_price, 2)
+
